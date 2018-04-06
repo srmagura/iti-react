22dc6057d9b20cde72a88f983ba67e9af7a6568c
Sam Magura <Sam.Magura@iticentral.com>
2018-04-06 15:43:45 -0400

    export validator core

--- Source/Website/Scripts/Util/ValidationLib/index.ts
+++ Source/Website/Scripts/Util/ValidationLib/index.ts
@@ -2,6 +2,7 @@
 export * from './InputWithFeedback';
 export * from './ValidatedInput';
 export * from './ReadOnlyInput';
+export * from './ValidatorCore';
 
 import * as _Validators from './Validators';
 export const Validators = _Validators;
