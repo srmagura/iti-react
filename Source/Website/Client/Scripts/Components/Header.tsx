789e5e34d593968b9c8fb61e29285de99626655a
Sam Magura <sam.magura@iticentral.com>
2018-04-21 14:51:46 -0400

    navbar

--- Source/Website/Client/Scripts/Components/Header.tsx
+++ Source/Website/Client/Scripts/Components/Header.tsx
@@ -5,15 +5,29 @@ interface IHeaderProps extends React.Props<any> {
 }
 
 export function Header(props: IHeaderProps): JSX.Element {
+    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">React SPA template</a>
+        <button className="navbar-toggler" type="button"
+            data-toggle="collapse"
+            data-target="#navbar-supported-content"
+            aria-controls="navbar-supported-content"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
 
-    return (
-        <nav id="header">
-            <div className="container">
-                <h2>React Template</h2>
-                <NavLink to="/">Index</NavLink>
-                <NavLink to="/page1">Page 1</NavLink>
-                <NavLink to="/page2">Page 2</NavLink>
-            </div>
-        </nav>
-    )
+        <div className="collapse navbar-collapse" id="navbar-supported-content">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/" exact>Index</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/page1">Page 1</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/page2">Page 2</NavLink>
+                </li>
+            </ul>
+        </div>
+    </nav>
 }
