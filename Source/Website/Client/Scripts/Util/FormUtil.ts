2ce4e9e69413c32125e983d2469c680c477ee8a5
Sam Magura <Sam.Magura@iticentral.com>
2018-04-24 15:27:28 -0400

    remove debounce

--- Source/Website/Client/Scripts/Util/FormUtil.ts
+++ Source/Website/Client/Scripts/Util/FormUtil.ts
@@ -11,27 +11,6 @@ export function formToObject(form: JQuery) {
     return obj
 }
 
-// from Underscore.js
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-export function debounce(func: any, wait: any, immediate?: any) {
-    var timeout: any
-    return function (this: any) {
-        const context = this
-        var args = arguments
-        const later = () => {
-            timeout = null
-            if (!immediate) func.apply(this, args)
-        }
-        const callNow = immediate && !timeout
-        clearTimeout(timeout)
-        timeout = setTimeout(later, wait)
-        if (callNow) func.apply(context, args)
-    }
-}
-
 export function nullToUndefined<T>(x: T | null | undefined): T | undefined {
     if (x == null) return undefined
     return x
