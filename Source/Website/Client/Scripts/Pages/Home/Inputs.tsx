12337d666f4174afa7688469d84682fed08586ad
Sam Magura <Sam.Magura@iticentral.com>
2018-05-21 10:25:05 -0400

    add validity label to inputs test

--- Source/Website/Client/Scripts/Pages/Home/Inputs.tsx
+++ Source/Website/Client/Scripts/Pages/Home/Inputs.tsx
@@ -2,16 +2,25 @@
 
 import { IPageProps } from 'Components/Routing/RouteProps';
 import { NavbarLink } from 'Components/Header';
-import { PhoneInput, Validators, TimeInput, requiredTimeValidator } from 'Util/ITIReact';
+import { PhoneInput, Validators, TimeInput, requiredTimeValidator, IFieldValidity, childValidChange } from 'Util/ITIReact';
+
+interface IValidityLabelProps extends React.Props<any> {
+    valid?: boolean
+}
+
+function ValidityLabel(props: IValidityLabelProps) {
+    return <span className="validity-label">{props.valid ? 'VALID' : 'INVALID'}</span>
+}
 
 interface IPageState {
-
+    fieldValidity: IFieldValidity
 }
 
 export class Page extends React.Component<IPageProps, IPageState> {
 
     state: IPageState = {
-
+        fieldValidity: {
+        }
     }
 
     componentDidMount() {
@@ -24,10 +33,14 @@ export class Page extends React.Component<IPageProps, IPageState> {
         })
     }
 
+    childValidChange(fieldName: string, valid: boolean) {
+        childValidChange(fieldName, valid, f => this.setState(f))
+    }
+
     render() {
         if (!this.props.ready) return null
 
-        const { } = this.state
+        const { fieldValidity } = this.state
 
         const showValidation = true
 
@@ -36,28 +49,31 @@ export class Page extends React.Component<IPageProps, IPageState> {
                 <div className="card-body">
                     <h5 className="card-title">Phone Input</h5>
                     <div className="form-group">
-                        <label>Not required</label>
+                        <label>Not required</label>{' '}<ValidityLabel valid={fieldValidity.phoneInput0} />
                         <PhoneInput
-                            name="myPhoneInput0"
+                            name="phoneInput0"
                             defaultValue=""
                             showValidation={showValidation}
-                            validators={[]} />
+                            validators={[]}
+                            onValidChange={valid => this.childValidChange('phoneInput0', valid)}/>
                     </div>
                     <div className="form-group">
-                        <label>Required</label>
+                        <label>Required</label>{' '}<ValidityLabel valid={fieldValidity.phoneInput1} />
                         <PhoneInput
-                            name="myPhoneInput1"
+                            name="phoneInput1"
                             defaultValue=""
                             showValidation={showValidation}
-                            validators={[Validators.required()]} />
+                            validators={[Validators.required()]}
+                            onValidChange={valid => this.childValidChange('phoneInput1', valid)}/>
                     </div>
                     <div className="form-group">
-                        <label>Invalid default value</label>
+                        <label>Invalid default value</label>{' '}<ValidityLabel valid={fieldValidity.phoneInput2} />
                         <PhoneInput
-                            name="myPhoneInput2"
+                            name="phoneInput2"
                             defaultValue="(919)555-271"
                             showValidation={showValidation}
-                            validators={[]} />
+                            validators={[]}
+                            onValidChange={valid => this.childValidChange('phoneInput2', valid)}/>
                     </div>
                 </div>
             </div>
@@ -65,20 +81,22 @@ export class Page extends React.Component<IPageProps, IPageState> {
                 <div className="card-body">
                     <h5 className="card-title">Time Input</h5>
                     <div className="form-group">
-                        <label>Not required</label>
+                        <label>Not required</label>{' '}<ValidityLabel valid={fieldValidity.timeInput0} />
                         <TimeInput
                             individualInputsRequired={false}
-                            name="myTimeInput0"
+                            name="timeInput0"
                             showValidation={showValidation}
-                            validators={[]} />
+                            validators={[]}
+                            onValidChange={valid => this.childValidChange('timeInput0', valid)}/>
                     </div>
                     <div className="form-group">
-                        <label>Required</label>
+                        <label>Required</label>{' '}<ValidityLabel valid={fieldValidity.timeInput1} />
                         <TimeInput
-                            name="myTimeInput1"
+                            name="timeInput1"
                             showValidation={showValidation}
                             individualInputsRequired={true}
-                            validators={[requiredTimeValidator()]} />
+                            validators={[requiredTimeValidator()]}
+                            onValidChange={valid => this.childValidChange('timeInput1', valid)}/>
                     </div>
                 </div>
             </div>
