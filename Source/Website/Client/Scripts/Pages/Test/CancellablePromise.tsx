0ac4bf7e3c278146ece48a6a757e344f641dc7c8
Sam Magura <sam.magura@iticentral.com>
2018-09-16 16:49:11 -0400

    Create CancellablePromise v2 (incomplete)

--- Source/Website/Client/Scripts/Pages/Test/CancellablePromise.tsx
+++ Source/Website/Client/Scripts/Pages/Test/CancellablePromise.tsx
@@ -7,8 +7,84 @@ import {
     Pager,
     ActionDialog,
     confirm,
-    ConfirmDialog
+    ConfirmDialog,
+    CancellablePromise
 } from '@interface-technologies/iti-react'
+
+function getPromise(
+    returnValue: string,
+    duration: number,
+    resolve = true
+): CancellablePromise<string> {
+    let timer: number | undefined
+
+    const promise = new Promise<string>((resolveFn, reject) => {
+        timer = setTimeout(() => {
+            if (resolve) {
+                resolveFn(returnValue)
+            } else {
+                reject('Promise rejected for reason XYZ.')
+            }
+        }, duration)
+    })
+
+    const cancel = () => {
+        clearTimeout(timer)
+    }
+
+    return new CancellablePromise(promise, cancel)
+}
+
+function beginTest(testName: string) {
+    console.log(`===== TEST: ${testName} =====`)
+}
+
+function endTest() {
+    console.log('SUCCESS')
+}
+
+function fail(reason: string) {
+    throw new Error('Test failed: ' + reason)
+}
+
+async function basic() {
+    beginTest('basic')
+
+    const p = getPromise('5', 500)
+    console.log('Created promise.')
+
+    const x = await p
+
+    if (x !== '5') fail(`Expected: '5'    Actual: '${x}'`)
+    endTest()
+}
+
+async function error() {
+    beginTest('error')
+
+    const p = getPromise('5', 500, false)
+    console.log('Created promise.')
+
+    try {
+        await p
+        fail('Promise did not reject.')
+    } catch (e) {
+        console.log(`Promise rejected with reason: "${e}".`)
+        endTest()
+    }
+}
+
+async function then() {
+    beginTest('then')
+
+    const p: CancellablePromise<number> = getPromise('5', 500).then(s => parseInt(s) * 2)
+    console.log('Created promise.')
+
+    const x = await p
+
+    if (x !== 10) fail(`Expected: 10    Actual: ${x}`)
+    endTest()
+}
 
 export class Page extends React.Component<IPageProps> {
     componentDidMount() {
@@ -24,9 +100,25 @@ export class Page extends React.Component<IPageProps> {
     render() {
         if (!this.props.ready) return null
 
+        const tests: [string, () => {}][] = [
+            ['Basic', basic],
+            ['Error', error],
+            ['Then', then]
+        ]
+
         return (
             <div>
                 <h1>Cancellable Promise Test</h1>
+                <p>Check the console to see the results of the tests.</p>
+                {tests.map((t, i) => {
+                    const [label, func] = t
+
+                    return (
+                        <button className="btn btn-secondary mr-3" onClick={func} key={i}>
+                            {label}
+                        </button>
+                    )
+                })}
             </div>
         )
     }
--- Source/iti-react/CancellablePromise.ts
+++ Source/iti-react/CancellablePromise.ts
@@ -26,3 +26,40 @@ export function withCancel<T>(
 export function cancellableResolve<T>(value: T): ICancellablePromise<T> {
     return withCancel(Promise.resolve(value), () => {})
 }
+
+//////////////////////////////////////////////////////////////////
+
+export class CancellablePromise<T> {
+    readonly promise: PromiseLike<T>
+    readonly cancel: () => void
+
+    constructor(promise: PromiseLike<T>, cancel: () => void) {
+        this.promise = promise
+        this.cancel = cancel
+    }
+
+    // This method is to allow you to perform a synchronous operation after the promise resolves.
+    // So the method does not allow onFulfilled to return a promise.
+    then<TResult>(
+        onFulfilled?: ((value: T) => TResult) | null,
+        onRejected?: ((reason: any) => TResult) | null
+    ): CancellablePromise<TResult> {
+        const resultPromise = this.promise.then(onFulfilled, onRejected)
+
+        return new CancellablePromise(resultPromise, this.cancel)
+    }
+}
+
+//// Use this to chain together multiple ICancellablePromises
+//export function withCancel<T>(
+//    promise: PromiseLike<T>,
+//    cancel: () => void
+//): ICancellablePromise<T> {
+//    ; (promise as any).cancel = cancel
+//    return promise as ICancellablePromise<T>
+//}
+
+//// analog of Promise.resolve. Returns a CancellablePromise that resolves with the given value
+//export function cancellableResolve<T>(value: T): ICancellablePromise<T> {
+//    return withCancel(Promise.resolve(value), () => { })
+//}
