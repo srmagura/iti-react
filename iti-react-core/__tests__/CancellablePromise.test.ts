b77976fb905cce7c974875c1fc97d22225d9ae29
Sam Magura <sam.magura@iticentral.com>
2019-08-14 09:56:57 -0400

    add test: simultaneous promises captured

--- iti-react-core/__tests__/CancellablePromise.test.ts
+++ iti-react-core/__tests__/CancellablePromise.test.ts
@@ -173,44 +173,72 @@ test('resolveVoid', async () => {
     }
 })
 
-test('pseudoCancellable: success', async () => {
-    try {
-        const number = await pseudoCancellable(Promise.resolve(1))
-        expect(number).toBe(1)
-    } catch (e) {
-        fail('Promise rejected.')
-    }
-})
-
-test('pseudoCancellable: cancelled', async () => {
-    try {
-        const promise = pseudoCancellable(
-            new Promise(resolve => setTimeout(resolve, 1000))
-        )
-        promise.cancel()
-        await promise
-
-        fail('Promise resolved when it should not have.')
-    } catch (e) {
-        expect(e).toBe(PSEUDO_PROMISE_CANCELED)
-    }
-})
-
-test('buildCancellablePromise: cancellation works', async () => {
-    const promise1Duration = 100
-
-    const overallPromise = buildCancellablePromise(async capture => {
-        await capture(getPromise('1', promise1Duration))
-
+describe('pseudoCancellable', () => {
+    test('pseudoCancellable: success', async () => {
         try {
-            await capture(getPromise('2', 3 * promise1Duration))
-            fail('Promise 2 resolved when it should have been canceled.')
-        } catch {}
+            const number = await pseudoCancellable(Promise.resolve(1))
+            expect(number).toBe(1)
+        } catch (e) {
+            fail('Promise rejected.')
+        }
     })
 
-    // Wait until promise1 resolves
-    await getPromise('', 2 * promise1Duration)
+    test('pseudoCancellable: cancelled', async () => {
+        try {
+            const promise = pseudoCancellable(
+                new Promise(resolve => setTimeout(resolve, 1000))
+            )
+            promise.cancel()
+            await promise
 
-    // This should cause promise2 to be canceled
-    overallPromise.cancel()
+            fail('Promise resolved when it should not have.')
+        } catch (e) {
+            expect(e).toBe(PSEUDO_PROMISE_CANCELED)
+        }
+    })
+})
+
+describe('buildCancellablePromise', () => {
+    test('cancellation works', async () => {
+        const promise1Duration = 100
+
+        const overallPromise = buildCancellablePromise(async capture => {
+            await capture(getPromise('1', promise1Duration))
+
+            try {
+                await capture(getPromise('2', 3 * promise1Duration))
+                fail('Promise 2 resolved when it should have been canceled.')
+            } catch {}
+        })
+
+        // Wait until promise1 resolves
+        await getPromise('', 2 * promise1Duration)
+
+        // This should cause promise2 to be canceled
+        overallPromise.cancel()
+    })
+
+    test('simultaneous promises captured', async () => {
+        const duration = 100
+
+        const overallPromise = buildCancellablePromise(async capture => {
+            const promise1 = capture(getPromise('1', duration))
+            const promise2 = capture(getPromise('2', duration))
+
+            try {
+                await promise1
+                fail('promise1 resolved')
+            } catch {}
+
+            try {
+                await promise2
+                fail('promise2 resolved')
+            } catch {}
+        })
+
+        overallPromise.cancel()
+
+        // Wait to see if either promise resolves
+        await getPromise('', duration * 2)
+    })
 })
