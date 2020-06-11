04072284fae5c4520de5f89e11375417672e97f0
Sam Magura <srmagura@gmail.com>
2020-06-11 10:59:56 -0400

    prettify one file

--- test-website/src/Pages/Test/UrlSearchParam.tsx
+++ test-website/src/Pages/Test/UrlSearchParam.tsx
@@ -10,9 +10,11 @@ interface PageState {
 
 export default function Page({ ready, onReady, history }: PageProps) {
     const onReadyRef = useRef(onReady)
-    useEffect(() => { onReadyRef.current = onReady })
+    useEffect(() => {
+        onReadyRef.current = onReady
+    })
 
-    useEffect(() => { 
+    useEffect(() => {
         const timer = window.setTimeout(() => {
             onReadyRef.current({
                 title: 'URL Search Param Test',
@@ -20,9 +22,10 @@ export default function Page({ ready, onReady, history }: PageProps) {
             })
         }, 1500)
 
-        return () => { window.clearTimeout(timer) }
+        return () => {
+            window.clearTimeout(timer)
+        }
     }, [])
-
 
     const location = useLocation()
     const params = new URLSearchParams(location.search)
@@ -30,27 +33,25 @@ export default function Page({ ready, onReady, history }: PageProps) {
 
     function addDigit(): void {
         const digit = Math.random().toString().charAt(3)
-        const newPath =
-            location.pathname +
-            formatUrlParams({ myParam: myParam + digit })
+        const newPath = location.pathname + formatUrlParams({ myParam: myParam + digit })
 
         history.push(newPath)
     }
 
-        if (!ready) return null
+    if (!ready) return null
 
-        return (
-            <div>
-                <h5 className="mb-3">Current param value: {myParam}</h5>
-                <p>
-                    <button className="btn btn-primary" onClick={addDigit}>
-                        Add a digit to param value
-                    </button>
-                </p>
-                <p>
-                    Should cause page to remount because location key will changed - see
-                    MyAsyncRouter.getLocationKey().
-                </p>
-            </div>
-        )
+    return (
+        <div>
+            <h5 className="mb-3">Current param value: {myParam}</h5>
+            <p>
+                <button className="btn btn-primary" onClick={addDigit}>
+                    Add a digit to param value
+                </button>
+            </p>
+            <p>
+                Should cause page to remount because location key will changed - see
+                MyAsyncRouter.getLocationKey().
+            </p>
+        </div>
+    )
 }
