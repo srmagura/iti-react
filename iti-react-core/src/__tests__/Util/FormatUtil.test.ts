fdd1bd2c89e6b64571006969f64666d22ac2b46c
Sam Magura <srmagura@gmail.com>
2020-10-07 10:44:46 -0400

    fix FormatUtil test

--- iti-react-core/src/__tests__/Util/FormatUtil.test.ts
+++ iti-react-core/src/__tests__/Util/FormatUtil.test.ts
@@ -5,7 +5,7 @@ describe('formatDollars', () => {
     expect(formatDollars(undefined)).toBe('')
     expect(formatDollars(12.0)).toBe('$12.00')
     expect(formatDollars(12.56)).toBe('$12.56')
-    expect(formatDollars(-0.56)).toBe('?$0.56')
+    expect(formatDollars(-0.56)).toBe('−$0.56')
 })
 
 test('formatPercent', () => {
@@ -13,7 +13,7 @@ test('formatPercent', () => {
     expect(formatPercent(undefined, 'display')).toBe('')
     expect(formatPercent(0.123456, 'display')).toBe('12.35%')
     expect(formatPercent(0.123456, 'display', 0)).toBe('12%')
-    expect(formatPercent(-0.123456, 'display')).toBe('?12.35%')
+    expect(formatPercent(-0.123456, 'display')).toBe('−12.35%')
     expect(formatPercent(0.46, 'display')).toBe('46%')
 
     expect(formatPercent(0.123456, 'userInput')).toBe('12.35')
