479b39f095f2e611c87b0d2def5510c34d5bbe78
Sam Magura <srmagura@gmail.com>
2021-08-27 11:55:20 -0400

    permissions updates

--- permissions/babel.config.js
+++
@@ -1,8 +0,0 @@
-// For Jest tests only
-module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-        '@babel/preset-react'
-    ],
-};
--- permissions/jest.config.js
+++ permissions/jest.config.js
@@ -1,6 +1,8 @@
 ﻿module.exports = {
     setupFiles: ['./src/__tests__/__helpers__/jest.setup.ts'],
 
+    transform: { "\\.tsx?$": "@sucrase/jest-plugin" },
+
     // ignore files that don't have test.ts or test.tsx extension (.e.g. __helpers__)
     testMatch: ["**/__tests__/**/*.test.ts?(x)"]
 }
