bdcd96dc816513a16d6e4423a2322b50f30ae668
Sam Magura <srmagura@gmail.com>
2021-08-27 12:07:09 -0400

    iti-react updates

--- iti-react/jest.config.js
+++ iti-react/jest.config.js
@@ -2,6 +2,8 @@
     testEnvironment: 'jsdom',
     transformIgnorePatterns: ["node_modules/(?!@interface-technologies)"],
 
+    transform: { "\\.tsx?$": "@sucrase/jest-plugin" },
+
     // ignore files that don't have test.ts or test.tsx extension (.e.g. __helpers__)
     testMatch: ['**/__tests__/**/*.test.ts?(x)']
 }
