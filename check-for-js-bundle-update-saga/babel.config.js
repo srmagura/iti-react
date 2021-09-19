c25beefa75f45683c9ff167b6c49556f053f2f1a
Sam Magura <srmagura@gmail.com>
2021-09-19 09:13:25 -0400

    babel.config.js

--- check-for-js-bundle-update-saga/babel.config.js
+++ check-for-js-bundle-update-saga/babel.config.js
@@ -3,6 +3,8 @@ module.exports = {
     presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript',
-        '@babel/preset-react',
+        ["@babel/preset-react", {
+            "runtime": "automatic"
+        }]
     ],
 }
