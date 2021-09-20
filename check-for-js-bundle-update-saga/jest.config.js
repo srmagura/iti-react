a4a4ef00a85fbfad5e1e605b226f8c1373939885
Sam Magura <srmagura@gmail.com>
2021-09-20 19:38:28 -0400

    fix tests

--- check-for-js-bundle-update-saga/jest.config.js
+++ check-for-js-bundle-update-saga/jest.config.js
@@ -4,6 +4,10 @@
     timers: 'real',
     resetMocks: true,
 
+    moduleNameMapper: {
+        '\\.css$': '<rootDir>/src/__tests__/__helpers__/CssStub.js',
+    },
+
     // ignore files that don't have test.ts or test.tsx extension (.e.g. __helpers__)
     testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 }
